0fc3a0b7afaadef6d910f4766eb1ddb44743c752
clearng <110361452+clearng@users.noreply.github.com>
2022-08-03 19:26:04 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,196 @@
+var data = [
+    {
+        name: 'search',
+        title: '搜索',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: '必应',
+                    logo: 'logo/bing.ico',
+                    url: 'https://cn.bing.com/search?q={0}'
+                },
+                {
+                    name: '百度',
+                    logo: 'logo/baidu.ico',
+                    url: 'https://www.baidu.com/s?wd={0}'
+                },
+                {
+                    name: '知乎',
+                    logo: 'logo/zhihu.ico',
+                    url: 'https://www.zhihu.com/search?type=content&q={0}'
+                },
+                {
+                    name: '微博',
+                    logo: 'logo/weibo.ico',
+                    url: 'https://s.weibo.com/weibo/{0}'
+                },
+                {
+                    name: '谷歌',
+                    logo: 'logo/google.ico',
+                    url: 'https://www.google.com/search?q={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'code',
+        title: '搜代码',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: 'csdn',
+                    logo: 'logo/csdn.ico',
+                    url: 'https://so.csdn.net/so/search?q={0}&t=blog'
+                },
+                {
+                    name: '博客园',
+                    logo: 'logo/博客园.ico',
+                    url: 'https://zzk.cnblogs.com/s?w={0}'
+                },
+                {
+                    name: 'github',
+                    logo: 'logo/github.ico',
+                    url: 'https://github.com/search?q={0}'
+                },
+                {
+                    name: 'StackOverflow',
+                    logo: 'logo/stack.ico',
+                    url: 'https://stackoverflow.com/search?q={0}'
+                }
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'video',
+        title: '搜视频',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: '腾讯视频',
+                    logo: 'logo/腾讯视频.ico',
+                    url: 'https://v.qq.com/x/search/?q={0}'
+                },
+                {
+                    name: '爱奇艺',
+                    logo: 'logo/爱奇艺.png',
+                    url: 'https://so.iqiyi.com/so/q_{0}'
+                },
+                {
+                    name: 'B站',
+                    logo: 'logo/哔哩哔哩.ico',
+                    url: 'https://search.bilibili.com/all?keyword={0}'
+                },
+                {
+                    name: '优酷',
+                    logo: 'logo/youku.ico',
+                    url: 'https://so.youku.com/search_video/q_{0}'
+                },
+                {
+                    name: '芒果',
+                    logo: 'logo/芒果.ico',
+                    url: 'https://so.mgtv.com/so?k={0}'
+                }
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'qrcode',
+        title: '二维码生成',
+        input_box: {
+            placeholder: '请输入二维码内容',
+            button: '生成',
+            if_blank: '请输入二维码内容',
+            logo: 'logo/qrcode.jpg',
+            execute: 'eval_extend_js',
+        },
+        extend_html: '<div id="qrcodearea" style="height:0;"><img src="" class="qrcode-img" style="width: 50%;border-radius: calc(var(--size)*6/100);"></div>',
+        extend_js: `document.querySelector("#qrcodearea").style.height="auto";
+        document.querySelector(".qrcode-img").src="https://api.pwmqr.com/qrcode/create/?url="+encodeURIComponent(this.content);`,
+    },
+    {
+        name: 'jiexi',
+        title: '视频解析',
+        input_box: {
+            placeholder: '请输入视频链接',
+            button: '解析',
+            if_blank: '请输入要解析的视频链接',
+            radios: [
+                {
+                    name: '解析1',
+                    logo: '',
+                    url: 'https://jx.parwix.com:4433/player/analysis.php?v={0}'
+                },
+                {
+                    name: '解析2',
+                    logo: '',
+                    url: 'https://jsap.attakids.com/?url={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'download',
+        title: '下载解析',
+        input_box: {
+            placeholder: '请输入下载链接',
+            button: '下载',
+            if_blank: '请输入要下载的文件链接',
+            radios: [
+                {
+                    name: 'github',
+                    logo: 'logo/github.ico',
+                    url: 'https://ghproxy.com/?q={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'copy',
+        title: '模板生成',
+        input_box: {
+            placeholder: '请输入插槽内容',
+            button: '复制',
+            if_blank: '请输入插槽内容',
+            radios: [
+                {
+                    name: '百度',
+                    logo: 'logo/baidu.ico',
+                    url: 'https://baidu.leeay.com/?s={0}'
+                },
+            ],
+            execute: 'copy',
+        },
+    },
+    {
+        name: 'show',
+        title: '展示文件',
+        input_box: {
+            placeholder: '请输入文件链接',
+            button: '展示',
+            if_blank: '请输入要展示的文件链接',
+            radios: [
+                {
+                    name: 'office',
+                    logo: 'logo/ms.ico',
+                    url: 'https://view.officeapps.live.com/op/view.aspx?src={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        },
+    },
+];
+
---
+++ js/base.js
@@ -0,0 +1,78 @@
+console.log(`项目地址：https://github.com/cxh1205/ToolStartPage
+欢迎star和fork!
+
+提示：
+双击图标可以清空输入框
+按“回车”可以直接访问目标页`);
+
+//字符串格式化
+String.prototype.format = function (args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+
+    var data = arguments; //如果模板参数是数组
+    if (arguments.length == 1 && typeof (args) == "object") {
+        //如果模板参数是对象
+        data = args;
+    }
+    for (var key in data) {
+        var value = data[key];
+        if (undefined != value) {
+            result = result.replace("{" + key + "}", value);
+        }
+    }
+    return result;
+};
+
+function copy(mes, transform = true) {
+    //创建input标签
+    var input = document.createElement('input');
+    //将input的值设置为需要复制的内容
+    if (transform) {
+        input.value = encodeURI(mes);
+    }
+
+    else {
+        input.value = mes;
+    }
+
+    //添加input标签
+    document.body.appendChild(input);
+    //选中input标签
+    input.select();
+    //执行复制
+    document.execCommand('copy');
+    //成功提示信息
+    alert(mes + '\n复制成功');
+    //移除input标签
+    document.body.removeChild(input);
+}
+
+function preload(logos) {
+    var images = new Array();
+    for (i = 0; i < logos.length; i++) {
+        images[i] = new Image();
+        images[i].src = logos[i];
+    }
+}
+var logos = [];
+data.forEach(block => {
+    if (block.input_box) {
+        if (block.input_box.logo) {
+            logos.push(block.input_box.logo);
+        }
+        if (block.input_box.radios) {
+            block.input_box.radios.forEach(radio => {
+                logos.push(radio.logo);
+            });
+        }
+    }
+});
+for (let i in data) {
+    for (let j in data[i]['radios']) {
+        logos.push(data[i]['radios'][j]['logo']);
+    }
+}
+preload(logos);
